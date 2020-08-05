9d9b537931ee366a5d786b1ec972fc32b410896a
Finn Hediger <finn.hediger@thurweb.ch>
2020-08-06 01:13:42 +0200

    first example added: files.sh

---
+++ examples/files.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Sorry for the long line.
+# Improvements are always welcome!
+
+# requires python
+# find . -not -path '*/\.*' | python -c "import sys as s;s.a=[];[setattr(s,'a',list(filter(lambda p: c.startswith(p+'/'),s.a)))or (s.stdout.write('\t'*len(s.a)+c[len(s.a[-1])+1 if s.a else 0:])or True) and s.a.append(c[:-1]) for c in s.stdin]" | navigator --stdin
+
+# only-shell version
+find . | tail +2 | sed -e "s/\.\///g" -e "s/[^-][^\/]*\//\t/g" | navigator --stdin
