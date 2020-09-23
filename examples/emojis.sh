e8c25c9db90197ea34a33801b7b9dbad03c780dd
Finn Hediger <finn.hediger@thurweb.ch>
2020-09-23 21:02:44 +0200

    updated examples to match the new INPUT style

--- examples/emojis.sh
+++ examples/emojis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # get all emojis
-emojify --list | tail -n +6 | navigator --stdin 2> /tmp/output
+emojify --list | tail -n +6 | navigator 2> /tmp/output
 
 cat /tmp/output | sed -e "s/:.*//g" | tr -d ' ' | tr -d '\n' | pbcopy
