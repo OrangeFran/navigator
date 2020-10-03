e7bb7da3eaf76613dfb8595a593f901447f3087f
Finn Hediger <finn.hediger@thurweb.ch>
2020-10-03 11:36:34 +0200

    new script to make use easier in chain of commands

---
+++ wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# wrapper around the cli to overcome usability issues
+# because of the stdin/stdout redirecting and the terminal user interface
+# which can somehow not be displayed at the same time
+# (hopefully a temporary workaround)
+
+# how this works:
+# it swaps standard out and standard
+# error so only stderr get's outputted to the next command
+navigator 3>&2 2>&1 1>&3 3>&-
