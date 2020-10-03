e3384b602629173f89e6aa3ba63c07068f3bba02
Finn Hediger <finn.hediger@thurweb.ch>
2020-10-03 11:50:07 +0200

    updated the example to use the wrapper script (copied to path under the name 'navi')

--- examples/emojis.sh
+++ examples/emojis.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # get all emojis
-emojify --list | tail -n +6 | sed 's/^ *//g' | navigator 2> /tmp/output
-
-cat /tmp/output | sed -e "s/:.*//g" | tr -d ' ' | tr -d '\n' | pbcopy
+emojify --list | tail -n +6 | sed 's/^ *//g' | navi | sed -e "s/:.*//g" | tr -d ' ' | tr -d '\n' | pbcopy
--- examples/files.sh
+++ examples/files.sh
@@ -7,4 +7,4 @@
 # find . -not -path '*/\.*' | python -c "import sys as s;s.a=[];[setattr(s,'a',list(filter(lambda p: c.startswith(p+'/'),s.a)))or (s.stdout.write('\t'*len(s.a)+c[len(s.a[-1])+1 if s.a else 0:])or True) and s.a.append(c[:-1]) for c in s.stdin]" | navigator
 
 # only-shell version
-find . | tail +2 | sed -e 's/^\.\///g' -e 's/[^-][^\/]*\//\\t/g' | navigator -s '\t'
+find . | tail +2 | sed -e 's/^\.\///g' -e 's/[^-][^\/]*\//\\t/g' | navi -s '\t'
