d2ed2f5c8113897ed7fb6158bf4261ca83b70ac9
Finn Hediger <finn.hediger@thurweb.ch>
2021-06-15 15:32:02 +0200

    Capitalise comments

--- wrapper.sh
+++ wrapper.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
-# wrapper around the cli to overcome usability issues
+# Wrapper around the cli to overcome usability issues
 # because of the stdin/stdout redirecting and the terminal user interface
 # which can somehow not be displayed at the same time
 # (hopefully a temporary workaround)
 
-# how this works:
-# it swaps standard out and standard
+# How this works:
+# It swaps standard out and standard
 # error so only stderr get's outputted to the next command
 navigator $@ 3>&2 2>&1 1>&3 3>&-
